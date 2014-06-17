23d54fccd876793b908e8f08c9277de91c50f16c
aktoca <aktoca1@gmail.com>
2014-06-17 00:44:22 -0400

    removed wrongly placed controller actions

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,10 +2,4 @@ class Movie < ActiveRecord::Base
   before_validation :set_default_description
   validates :movie_title, :year, :director, :description, presence: true
 
-
-  def set_default_description
-    if self[:description] == nil
-      self[:description] = 'Not avaliable'
-    end
-  end
 end
