e0fd46f2a2cbebcbfa08ccb40bbed863d6c426ee
aktoca <aktoca1@gmail.com>
2014-06-23 15:16:02 -0400

    Romoved validation for description presence true

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,4 @@
 class Movie < ActiveRecord::Base
-  before_validation :set_default_description
-  validates :movie_title, :year, :director, :description, presence: true
+  validates :movie_title, :year, :director, presence: true
 
 end
