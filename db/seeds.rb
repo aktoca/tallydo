e7712c7948b53d4b5eac4ee2a771ac893fa75c54
aktoca <aktoca1@gmail.com>
2014-07-19 21:35:44 -0400

    Create two basic movie seeds to start

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ movies =[ 
+             {
+  movie_title: "A Trip to the Moon",
+  aka_title:"Le Voyage Dans La Lune",
+  year: 1902,
+  director: "Georges Méliès",
+  imdbid:'tt0000417'
+}, {
+  movie_title: "The Great Train Robbery",
+  year: 1903,
+  director: "Edwin S. Porter",
+  imdbid:'tt0000439'
+}]
+
+movies.each do |movie|
+  Movie.create(movie)
+end
+
