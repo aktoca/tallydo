fc60ed68da771a96f3f97966899aa6736fdfb05e
aktoca <aktoca1@gmail.com>
2014-07-19 21:29:09 -0400

    Succesful sign-up redirects to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :require_login, only: [:index, :new, :create]
+
   def new
     @user = User.new
   end
@@ -7,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(:users, notice: 'User was successfully created')
+      redirect_to(new_session_path, notice: 'User was successfully created')
     else
       render 'new'
     end
