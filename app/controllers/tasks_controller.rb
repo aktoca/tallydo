8573735359b50e2075cb0bd5b0f139f6c979f9a8
aktoca <aktoca1@gmail.com>
2014-07-19 21:22:53 -0400

    Added ugly user tasks display with button to edit Tasks.done. Set a placeholder for Tasks#update in controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,15 @@ class TasksController < ApplicationController
   def new
   end
 
+def update
+  redirect_to :back, notice: "Update 'seen it'"
+end
+
   def delete
   end
 
   def index
-      @user = User.first
+      @user = User.find(params[:user_id])
+      @me = current_user
   end
 end
