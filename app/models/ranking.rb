e8c3273f2439bfb639c294f4a0685472b3621e7c
aktoca <aktoca1@gmail.com>
2014-08-21 03:25:24 -0400

    Added scopes to tasks model for tasks#index. Added 'belongs_to :movie' to task and ranking models for .includes, but may delete later

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -2,6 +2,8 @@ class Ranking < ActiveRecord::Base
   validates :doable_id, :doable_type, :list_id, presence: true 
 
   belongs_to :doable, polymorphic: true
+  belongs_to :movie, -> { where(movies: {doable_type: 'Movie' }) }, foreign_key: 'doable_id'
   belongs_to :list
+
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
+  scope :todo,-> { where(done: false)}
+  scope :tallied,-> { where(done: true)}
+  scope :mine, lambda {|user| where(user_id: user.id)}
+  scope :by_doable,lambda {|type|where(doable_type: type)}
+  scope :percent_done, lambda {|user| (mine(user).tallied.length / mine(user).length) * 100} 
+
   belongs_to :doable, polymorphic: true
+  belongs_to :movie, -> { where(movies: {doable_type: 'Movie' }) }, foreign_key: 'doable_id'
   belongs_to :user
+
+
 end
