ff3087a9b282542955c62359e37daf77af08eb56
aktoca <aktoca1@gmail.com>
2014-08-21 03:32:03 -0400

    Added basic CRUD to ranking controller - tbd

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,2 +1,28 @@
 class RankingController < ApplicationController
+
+  def new
+    @rank = Ranking.new
+  end
+
+  def create
+    @rank = Ranking.new(rank_params)
+    if @rank.save
+      redirect_to(new_session_path, notice: 'Rank successful')
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @rank = Ranking.find(params[:id])
+  end
+
+
+  private 
+  def rank_params
+    params.require(:ranking).permit(:id, :doable_id, :doable_type, :list_id, :rank) 
+
+  end
+
+end
 end
