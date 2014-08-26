ca2140f53c4ceebdbdacbf4736f4540af8c8b79d
aktoca <aktoca1@gmail.com>
2014-08-26 17:39:44 -0400

    Connected Lists to Movies. Now link with 'List.movies'

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   validates :name, :year, :creator, presence: true
 
   has_many :rankings 
-  has_many :movies, through: :rankings
+   has_many :movies, through: :rankings, source: :doable, source_type: "Movie" 
   has_and_belongs_to_many :users
   
 end
