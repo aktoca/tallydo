d08c7ffade2c0d892521f9132991e2eaf804cd18
aktoca <aktoca1@gmail.com>
2014-09-09 18:47:41 -0700

    Added 'or zero' to the scope percentage, so no div by zero error'

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ActiveRecord::Base
   scope :mine, lambda {|user| where(user_id: user.id)}
   scope :by_doable,lambda {|type|where(doable_type: type)}
   scope :by_complete,lambda {|complete|where(done: complete)}
-  scope :percent_done, lambda {|user| (mine(user).tallied.length / mine(user).length) * 100} 
+  scope :percent_done, lambda {|user| mine(user).length > 0 ? (mine(user).tallied.length / mine(user).length) * 100  :0 } 
 
   belongs_to :doable, polymorphic: true
   belongs_to :movie, -> { where(movies: {doable_type: 'Movie' }) }, foreign_key: 'doable_id'
